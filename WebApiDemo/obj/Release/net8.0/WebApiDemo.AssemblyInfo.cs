6da6b9c3449e04529815b586ddcafd5a23dc1904
Pradeep Yazamanam <pradeep_yazamanam@epam.com>
2026-06-04 15:56:47 +0530

    Remove build artifacts from source control

--- WebApiDemo/obj/Release/net8.0/WebApiDemo.AssemblyInfo.cs
+++ WebApiDemo/obj/Release/net8.0/WebApiDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72004e476c0f3386abb1fc6e07b40c997b5a8c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eaadd414ee2aa002e2f4fd78fb28bb85837982d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
